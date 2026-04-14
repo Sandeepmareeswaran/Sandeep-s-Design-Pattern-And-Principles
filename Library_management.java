0a1c95ee5f1b7c1150cc397f3606135d93e9c21e
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-04-14 19:32:38 +0530

    book class implemente

--- Library_management.java
+++ Library_management.java
@@ -1,13 +1,31 @@
 /*Design a Java-based Library Management System where users can borrow and return books, 
 and the system tracks availability and issued books. */
 import java.util.*;
-class Library{
+class Book{
     
     String book_name;
     String author_name;
     boolean avail;
+    Book(String book_name,String author_name){
+        this.book_name=book_name;
+        this.author_name=author_name;
+        this.avail = true;
+    }
 
-    
+    String getName(){
+        return book_name;
+    }
+
+    boolean isAvail(){
+        return avail;
+    }
+
+    void setAvail(boolean status){
+        avail=state;
+    }
+
+
+
 }
 
 class Library_management{

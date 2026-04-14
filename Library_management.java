5385ec83faca94dfdea62c076f972357f92aed56
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-04-14 18:36:06 +0530

    created the classes

--- Library_management.java
+++ Library_management.java
@@ -1,2 +1,17 @@
 /*Design a Java-based Library Management System where users can borrow and return books, 
 and the system tracks availability and issued books. */
+import java.util.*;
+class Library{
+    
+    String book_name;
+    String author_name;
+    boolean avail;
+
+    
+}
+
+class Library_management{
+    public static void main(String[] args){
+
+    }
+}

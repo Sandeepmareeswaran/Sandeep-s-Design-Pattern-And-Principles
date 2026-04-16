ff62e21082e69429ee8e6ba20a9dc127e72ade9c
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-04-17 00:18:09 +0530

    multi level intraction implemented

--- Object_communication.java
+++ Object_communication.java
@@ -28,14 +28,38 @@ class person{
     }
 }
 
+class Company{
+    ArrayList<person> person_list;
+    Company(){
+        person_list=new ArrayList<>();
+    }
+    void personAdd(person p){
+        person_list.add(p);
+    }
+
+    void listPerson(){
+        for(person lst:person_list){
+            System.out.println("Person name: "+lst.name);
+            lst.showCars();
+        }
+    }
+
+
+}
+
 class Object_communication{
     public static void main(String[] args){
         car c1 = new car("scoda");
         car c2=new car("swift");
+        Company c=new Company();
+
 
         person p1 = new person("Sandeep");
         p1.addCar(c1);
         p1.addCar(c2);
         p1.showCars();
+
+        c.personAdd(p1);
+        c.listPerson();
     }
 }

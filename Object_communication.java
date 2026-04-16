37f0f09f2790f880d25c1f99e537e36e0b3d528f
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-04-17 00:09:07 +0530

    Object communication added

--- Object_communication.java
+++ Object_communication.java
@@ -1,28 +1,41 @@
+import java.util.*;
+class car{
+    String car_name;
+    car(String car_name){
+        this.car_name=car_name;
+    }
+
+}
 class person{
     String name;
+    ArrayList<car> cars;
+
     person(String name){
         this.name=name;
+        cars = new ArrayList<>();
+        
     }
 
-    void get_car(car c){
-        System.out.print("car named for person is"+c.carname);
+    void addCar(car c){
+        cars.add(c);
     }
-}
-class car{
-    String carname;
-    car(String carname){
-        this.carname=carname;
-    }
+    void showCars(){
+        for(car ca:cars){
+            System.out.println(ca.car_name);
 
+        }
+        
+    }
 }
+
 class Object_communication{
     public static void main(String[] args){
-        car c1=new car("scooda");
-        person p1=new person("Sandeep");
+        car c1 = new car("scoda");
+        car c2=new car("swift");
 
-        p1.get_car(c1);
-
-
-
+        person p1 = new person("Sandeep");
+        p1.addCar(c1);
+        p1.addCar(c2);
+        p1.showCars();
     }
 }

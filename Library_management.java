526eb7b7bf4ead2665554e50eb1fa88d9c29cd90
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-04-18 23:56:43 +0530

    have commited the bridge to the add the person and add the book

--- Library_management.java
+++ Library_management.java
@@ -6,7 +6,7 @@ class Book{
     Book(String book_name,String author){
         this.book_name=book_name;
         avail=true;
-        this.autohor=author;
+        this.author=author;
     }
 }
 class Person{
@@ -25,11 +25,21 @@ class Person{
 }
 class Library{
     ArrayList<Person> pr;
+    ArrayList<Book> bn;
     Library(){
         pr=new ArrayList<>();
+        bn=new ArrayList<>();
     }
 
-    void addPerson(Person p){
+    void addBook(String bookname,String author){
+        Book b=new Book(bookname,author);
+        bn.add(b);
+        System.out.println("Book added !!");
+    }
+
+
+    void addPerson(String personname){
+        Person p=new Person(personname);
         pr.add(p);
         System.out.println("Person Added !!");
     }
@@ -38,18 +48,33 @@ class Library_management{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int state;
+        Library lib = new Library();
         do{
             System.out.println("press 1=> add book\n press 2=> add person");
             state = scan.nextInt();
 
             switch(state){
                 case 1:
+                    scan.nextLine();
                     System.out.println("Enter the book name");
                     String bkname=scan.nextLine();
-                    Book b = new Book()
+                    System.out.println("Enter the author name !!");
+                    String autname=scan.nextLine();
+                    
+                    lib.addBook(bkname,autname);
+                    break;
+                case 2:
+                    scan.nextLine();
+                    System.out.println("Enter the name of the person to be added !!");
+                    String personname = scan.nextLine();
+                    lib.addPerson("Sandeep");
+                    break;
+                default:
+                    System.out.println("Enter the valid one !!");
+
             }
 
-        }
+        }while(state!=0);
 
         
     }

512cd0d5996a3f82a15ff5e554fac50957ce7cb3
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-04-25 21:26:18 +0530

    user and course adding content added

--- Student_enrolement.java
+++ Student_enrolement.java
@@ -10,20 +10,31 @@ class course{
 }
 class student{
     String name;
-    ArrayList<course> clst = new ArrayList<>();
+    ArrayList<course> clst;
     student(String name){
         this.name=name;
+        clst = new ArrayList<>();
     }
 }
-class system{
+class SystemManager{
     ArrayList<student> slist = new ArrayList<>();
     ArrayList<course> clist = new ArrayList<>();
+    void addStudent(String name){
+        student s1 = new student(name);
+        slist.add(s1);
+        System.out.println("Student Added !!");
+    }
+    void addCourse(String name){
+        course c1 = new course(name);
+        clist.add(c1);
+        System.out.println("Course Added !!");
+    }
 }
 class Student_enrolement{
     public static void main(String[] arg){
         Scanner scan = new Scanner(System.in);
         int state;
-        system sys = new system();
+        SystemManager sys = new SystemManager();
         do{
             System.out.println("Press 1 => add student\n Press 2 => add course\n Press 3 => add the course to the student\n");
             state = scan.nextInt();
@@ -31,7 +42,21 @@ class Student_enrolement{
             switch(state){
                 case 0:
                     System.out.println("Exiting . . .");
-
+                    break;
+                case 1:
+                    scan.nextLine();
+                    System.out.println("Enter the Student name that to be added: ");
+                    String studentname = scan.nextLine();
+                    sys.addStudent(studentname);
+                    break;
+                case 2:
+                    scan.nextLine();
+                    System.out.println("Enter the Course name that to be added: ");
+                    String coursename =  scan.nextLine();
+                    sys.addCourse(coursename);
+                    break;
+                default:
+                    System.out.println("Enter the valid one");
             }
         }while(state!=0);
         

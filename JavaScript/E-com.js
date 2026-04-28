3bdc368a1b9f84d4c1e4f107b757a3dba6057f28
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-04-28 23:24:17 +0530

    design for basic e-com system added

---
+++ JavaScript/E-com.js
@@ -0,0 +1,41 @@
+//Design a system where a user can add products to a cart, view items, and calculate total price.
+var users={};
+var products={};
+
+class User{
+
+    constructor(name){
+        this.name=name;
+    }
+
+    addUser(){
+        users[this.name]={
+            name:this.name,
+            cart:[]
+        }
+        console.log(this.name);
+    }
+    showUser(){
+        console.log(users);
+    }
+}
+class Product{
+    addProduct(id,price){
+        products[id]={
+            id:id,
+            price:price
+        }
+    }
+    showProduct(){
+       console.log(products);
+    }
+}
+let u1=new User("sandeep");
+u1.addUser();
+let p1 = new Product();
+p1.addProduct(1,100);
+p1.addProduct(2,200);
+p1.showProduct();
+u1.showUser();
+
+

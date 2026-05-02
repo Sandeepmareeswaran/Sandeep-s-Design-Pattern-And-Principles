42d78ae45d853b9235fd3834930c9d1baea42f09
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-05-02 19:00:42 +0530

    added the vechile to the slots

--- JavaScript/parkingslots.js
+++ JavaScript/parkingslots.js
@@ -1,5 +1,39 @@
 //Manage parking slots for vehicles.
 var slots = [];
+var vlist = [];
+class Vehicle{
+    
+    constructor(name,type){
+        this.number=name;
+        this.type=type;
+        this.veh={
+            VehicleName:this.number,
+            Type:this.type
+        }
+        vlist.push(this.veh);
+        console.log(vlist);
+    }
+
+    bookSlot(number){
+        var bkslot = slots[number-1];
+        console.log(bkslot);
+        if(bkslot["Vehicles"]===null){
+            slots[number]["Vehicles"]=this.veh;
+            console.log(slots);
+        }else{
+            console.log("this slot already bookek !!");
+        }    
+    }
+
+    removeSlot(){
+        for(let i=0;i<slots.length;i++){
+            let tem = slots[i];
+            if(tem["Vehicles"]===this.veh){
+                slots[i]["Vehicles"]=null;
+            }
+        }
+    }
+}
 
 function createSlots(n){
     for(let i=1;i<=n;i++){
@@ -12,7 +46,7 @@ function createSlots(n){
 function seeAvailSlots(){
     for(let i=0;i<slots.length;i++){
         var temp = slots[i];
-        if(temp["Vehicles"]==null){
+        if(temp["Vehicles"]===null){
             console.log("Slots numbers: "+temp["Number"]);
         }
       
@@ -20,5 +54,9 @@ function seeAvailSlots(){
     }
 }
 createSlots(10);
-console.log(slots[1]);
+
+
+
+let v1 = new Vehicle("TN 15 MS 2422","4wheel");
+v1.bookSlot(2);
 seeAvailSlots();

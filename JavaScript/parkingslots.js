399caff3161832319514ca8bd1780c650f5bb2a8
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-05-02 18:14:51 +0530

    added the slots structure creation

--- JavaScript/parkingslots.js
+++ JavaScript/parkingslots.js
@@ -1,1 +1,24 @@
 //Manage parking slots for vehicles.
+var slots = [];
+
+function createSlots(n){
+    for(let i=1;i<=n;i++){
+        slots.push({Number:i,Vehicles:null});
+    }
+    console.log("Slots created !!");
+    
+}
+
+function seeAvailSlots(){
+    for(let i=0;i<slots.length;i++){
+        var temp = slots[i];
+        if(temp["Vehicles"]==null){
+            console.log("Slots numbers: "+temp["Number"]);
+        }
+      
+
+    }
+}
+createSlots(10);
+console.log(slots[1]);
+seeAvailSlots();

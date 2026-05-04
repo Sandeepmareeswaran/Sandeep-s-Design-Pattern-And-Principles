b5aaf612e21c828fbfb2bddad896a3ae80c044a6
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-05-04 23:00:00 +0530

    have commited the parking basic structure and functionalities

--- JavaScript/ParkingSlotUp.js
+++ JavaScript/ParkingSlotUp.js
@@ -2,6 +2,44 @@
 
 let slot = [];
 
+class Vechicle{
+    constructor(num,type){
+        this.num=num;
+        this.type=type;
+        this.detail={
+            Number:this.num,
+            Type:this.type
+        }
+
+        
+    }
+    parkVechicle(){
+        var f=false;
+        for(let i=0;i<slot.length;i++){
+            
+            if(slot[i]["Type"]==this.type && slot[i]["Vehicle"]===null){
+                slot[i]["Vehicle"]=this.detail;
+                console.log("vehicle parked in the number called"+slot[i]["Number"]);
+                f=true;
+                break;
+            }
+        }
+        if(!f){
+            console.log("No space to park !!!");
+        }
+    }
+    removePark(){
+        
+    }
+}
+
+function showAvail(Type){
+    for(let i=0;i<slot.length;i++){
+        if(slot[i]["Type"]==Type && slot[i]["Vehicle"]===null){
+            console.log("Avail park "+slot[i]["Number"]);
+        }
+    }
+}
 function createSlots(n){
     for(let i=1;i<=n;i++){
         var slots={};
@@ -19,3 +57,9 @@ function createSlots(n){
     console.log(slot);
 }
 createSlots(10);
+
+var b1=new Vechicle('TN 01 SM 20202','Bike');
+b1.parkVechicle();
+b1.removePark();
+showAvail("Bike");
+showAvail("Car");

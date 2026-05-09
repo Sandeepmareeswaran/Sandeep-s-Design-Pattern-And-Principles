287dead374dd778f0065c535e0e1e4734a9abd34
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-05-09 12:40:12 +0530

    Added tree implementation voer the java

---
+++ Tharavu amaipugal maanakumurai/Trees.java
@@ -0,0 +1,47 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+class Trees{
+    Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        if(root.data<data){
+            root.right=insert(root.right,data);
+        }else{
+            root.left=insert(root.left,data);
+        }
+        return root;
+    }
+
+    void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+
+
+    public static void main(String args[]){
+        Trees tree = new Trees();
+
+        Node root =null;
+        root=tree.insert(root,10);
+        root=tree.insert(root,50);
+        root=tree.insert(root,30);
+        root=tree.insert(root,20);
+        root=tree.insert(root,60);
+        tree.inorder(root);
+
+    }
+}

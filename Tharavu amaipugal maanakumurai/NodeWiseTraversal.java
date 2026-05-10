4ab04706c6ffb94c382d63e827265c0f3ae9806d
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-05-10 20:59:08 +0530

    added the node wise traversal from top to botom complted

--- Tharavu amaipugal maanakumurai/NodeWiseTraversal.java
+++ Tharavu amaipugal maanakumurai/NodeWiseTraversal.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Node{
     int val;
     Node right;
@@ -10,6 +11,29 @@ class Node{
 }
 
 class NodeWiseTraversal{
+
+    void ToptoBottomDisplay(Node root){
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int s = q.size();
+            for(int i=0;i<s;i++){
+                Node cur = q.poll();
+                System.out.print(cur.val+" ");
+                if(cur.left!=null){
+                    q.add(cur.left);
+                }
+                if(cur.right!=null){
+                    q.add(cur.right);
+                }
+
+            }
+
+        }
+        System.out.println("Completed display !!");
+
+    }
     Node insert(Node root,int val){
         if(root==null){
             return new Node(val);
@@ -28,11 +52,13 @@ class NodeWiseTraversal{
         NodeWiseTraversal tree = new NodeWiseTraversal();
 
         Node root = null;
-        root=tree.insert(root,10);
+        root=tree.insert(root,25);
         root=tree.insert(root,20);
         root=tree.insert(root,30);
-        root=tree.insert(root,40);
+        root=tree.insert(root,10);
         root=tree.insert(root,50);
+
+        tree.ToptoBottomDisplay(root);
 
 
     }

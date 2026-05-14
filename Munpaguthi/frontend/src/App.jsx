39aa3a009febc489fee6da77faf703eceda12553
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-05-14 18:31:46 +0530

    have connected the post and get at munpaguthi

--- Munpaguthi/frontend/src/App.jsx
+++ Munpaguthi/frontend/src/App.jsx
@@ -2,6 +2,9 @@ import {useEffect,useState} from 'react';
 import axios from 'axios';
 
 function App(){
+  const[patientName,setpatientName] = useState("");
+  const[age,setAge]=useState("");
+  const[des,setDes]=useState(""); 
   const[bookings,setBookings]=useState([]);
   useEffect(()=>{
     axios.get("http://localhost:4000/api/showbook")
@@ -11,8 +14,57 @@ function App(){
     .catch((error)=>{
       console.log(error);
     })
-  })
+  },[])
+  
+
+  const handleSubmit= async (e)=>{
+    e.preventDefault();
+    try{
+      const res =await axios.post(
+        'http://localhost:4000/api/bookapp',
+        {
+          patientName,
+          age,
+          des
+        }
+      );
+      setBookings([...bookings,res.data]);
+      setpatientName("");
+      setAge("");
+      setDes("");
+
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  
   return(
+    <div>
+      <form
+      onSubmit={handleSubmit}>
+        <input
+        value={patientName}
+        placeholder="Enter the patient name"
+        onChange={(e)=>setpatientName(e.target.value)}
+        />
+        <input
+        value={age}
+        placeholder="Enter the age"
+        onChange={(e)=>setAge(e.target.value)}
+        />
+        <input
+        value={des}
+        placeholder="Enter the des"
+        onChange={(e)=>setDes(e.target.value)}
+        />
+        <button type='submit'>Submit</button>
+
+
+      </form>
+    <div>
+
+    </div>
     <div>
       <h1>Bookings Appointments are listed bellow!!</h1>
       {
@@ -23,6 +75,7 @@ function App(){
           </div>
         ))
       }
+    </div>
     </div>
 
   );

7ad8dd90f784c9f60bb0fbc81f655bd72736ab7c
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-06-04 20:38:03 +0530

    have completed the muthal amaipu vadivam in mlai pambu

--- MalaiPambu/system1.py
+++ MalaiPambu/system1.py
@@ -18,15 +18,25 @@ class StudentManagement:
     def displayStudent(self):
         for i in self.studentlist:
             print(i.display())
+    def searchStudent(self):
+        searchname=input("Enter the name of the student to search !")
+        for i in self.studentlist:
+            if(i.name==searchname):
+                print("student found")
+                i.display()
+        
     def menu(self):
         while True:
-            print("enter 1 to add student: \n enter 2 for display list")
+            print("enter 1 to add student: \n enter 2 for display list \n search")
 
             opt = int(input())
             if(opt==1):
                 self.addStudent()
             elif(opt==2):
                 self.displayStudent()
+            elif(opt==3):
+                self.searchStudent()
+
             else:
                 print("terminated !")
                 break

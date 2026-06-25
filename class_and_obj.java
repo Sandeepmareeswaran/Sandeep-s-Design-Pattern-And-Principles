d43583a41b3abb466c23e44296fc7d7b10f11eb3
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-06-25 22:53:51 +0530

    added things in teh class and objedcts

--- class_and_obj.java
+++ class_and_obj.java
@@ -18,3 +18,23 @@ public class class_and_obj{
 
     }
 }
+class details{
+    String name;
+    int age;
+    details(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+    void display(){
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+    }
+
+}
+public class class_and_obj{
+    public static void main(String[] args){
+        details obj = new details("Sandeep",21);
+        obj.display();
+
+    }
+}
